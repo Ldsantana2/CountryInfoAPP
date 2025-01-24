3127dd1cf69ad24c5844fc237a5178b5a5997c69
Lucas <ldsantana_2@hotmail.com>
2025-01-24 11:19:51 -0300

    back end finalizado

--- src/controladores/controladores.ts
+++ src/controladores/controladores.ts
@@ -20,36 +20,50 @@ export const countryInfo = async (req: Request, res: Response) => {
     const { countryCode } = req.params
 
     try {
-        const borderResponse = await axios.get(
+        const countryData = await axios.get(
             `https://date.nager.at/api/v3/CountryInfo/${countryCode}`
         )
-        const borderCountries = borderResponse.data.borders
 
-        const populationResponse = await axios.get(
-            'https://countriesnow.space/api/v0.1/countries/population'
+        const countryName = countryData.data.commonName
+
+        const borderCountries = countryData.data.borders.map(
+            (border: {
+                commonName: any
+                officialName: any
+                countryCode: any
+                region: any
+            }) => {
+                const { commonName, officialName, countryCode, region } = border
+                return { commonName, officialName, countryCode, region }
+            }
         )
-        const flagResponse = await axios.get(
-            'https://countriesnow.space/api/v0.1/countries/flag/images'
+
+        const populationResponse = await axios.post(
+            'https://countriesnow.space/api/v0.1/countries/population',
+            {
+                country: countryName,
+            }
         )
 
         const populationData = populationResponse.data.data
+
+        const flagResponse = await axios.post(
+            'https://countriesnow.space/api/v0.1/countries/flag/images',
+            {
+                country: countryName,
+            }
+        )
+
         const flagData = flagResponse.data.data
 
-        const populationInfo = populationData.filter((item: any) => {
-            return item.code === countryCode
-        })
-
-        const flagInfo = flagData.filter((item: any) => {
-            return item.code === countryCode
-        })
-
-        const countryInfo = {
+        const response = {
+            name: countryName,
             borders: borderCountries,
-            population: populationInfo,
-            flagUrl: flagInfo,
+            population: populationData,
+            flagUrl: flagData,
         }
 
-        return res.status(200).json(countryInfo)
+        return res.status(200).json(response)
     } catch (error) {
         console.error('Error fetching country info:', error)
 
